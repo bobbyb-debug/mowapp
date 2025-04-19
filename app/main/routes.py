27e20c25b18449e3e47f6d4c021262e26616f236
bobbyb-debug <b.burk2007@gmail.com>
2025-04-19 12:55:05 -0500

    Add /initdb route for DB initialization

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,11 +2,11 @@ from flask import render_template, redirect, url_for
 from app.main import bp
 from app import db
 
+@bp.route('/')
+def home():
+    return redirect(url_for('main.initdb'))  # temporary: auto-redirect for testing
+
 @bp.route('/initdb')
 def initdb():
     db.create_all()
     return "<h3>✅ Database initialized successfully!</h3>"
-
-@bp.route('/')
-def home():
-    return redirect(url_for('main.initdb'))  # redirect to /initdb just for testing
